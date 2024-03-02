60f7cdab8a69ea566b8bbf77355d7294c08ac49a
Radu <radu.g.paraian@gmail.com>
2024-03-02 13:03:24 +0000

    Initial Commit

---
+++ lib/dice_roller.dart
@@ -0,0 +1,49 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+final randomizer = Random();
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var currentDiceRoll = 1;
+
+  void rollDice() {
+    setState(() {
+      currentDiceRoll = randomizer.nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/images/dice-$currentDiceRoll.png',
+          width: 200,
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        ElevatedButton(
+          onPressed: rollDice,
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.deepPurple,
+            textStyle: const TextStyle(
+              fontSize: 15,
+            ),
+          ),
+          child: const Text('Roll Dice'),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:roll_dice/gradient_container.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(
+          Colors.deepPurple,
+          Colors.yellow,
+        ),
+      ),
+    ),
+  );
+}
